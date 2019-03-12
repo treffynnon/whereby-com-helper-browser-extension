a7074ff2edeb63f6cc90ea3c6656014ad002dccb
Simon Holywell <simon@holywell.com.au>
2019-03-13 09:27:34 +1000

    shorten corporate names

--- src/utils/appear.in.ts
+++ src/utils/appear.in.ts
@@ -1,13 +1,14 @@
 export const isAppearInUri = (uri: string): boolean => /appear\.in\/.+/.test(uri);
-export const appearInUriToRoomName = (uri: string): string =>
-  uri.trim().replace(/^(https?:\/\/)?appear\.in\//, '');
-export const ensureHttpPrepended = (uri: string): string => {
+export const isStandardUri = (uri: string): boolean => /https?:\/\/appear.in\//.test(uri);
+export const isCorporateSubdomain = (uri: string): boolean => isAppearInUri(uri) && !isStandardUri(uri);
+export const isCorporateShortname = (uri: string): boolean => /^[^>]+>.+/.test(uri);
+export const appearInUriToRoomName = (uri: string): string => {
   const x = uri.trim();
-  return (/^https?:\/\//).test(x)
-  ? x
-  : `https://${x}`;
-}
+  return isCorporateSubdomain(x)
+    ? x.replace(/^(https?:\/\/)?([^.]+)\.appear\.in\//, '$2 > ')
+    : x.replace(/^(https?:\/\/)?appear\.in\//, '');
+};
 export const buildAppearInUrl = (room: string): string =>
-  isAppearInUri(room)
-  ? ensureHttpPrepended(room)
+  isCorporateShortname(room)
+  ? room.replace(/^([^\s]+) > (.+)/, `https://$1.appear.in/$2`)
   : `https://appear.in/${room}`;
